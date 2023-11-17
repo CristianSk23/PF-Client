cf65fd031d04a85e940ff7087d4f7047f8d4c0c6
barufe <belkisarufe@gmail.com>
2023-11-17 15:23:55 -0500

    Missing initial settings are added

---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  allProducts: [],
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
+export default reducer;
