3ed4e807f1a84ee18998b31d36e517e8b592d3ae
diegozallocco <diegozallocco@hotmail.com>
2023-11-29 00:13:00 -0300

    Deploy configuration to comunicate with PF-Server

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client'
 import store from './redux/store/store.js';
+import axios from "axios";
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
+
+  // url general
+  // axios.defaults.baseURL = "http://localhost:3001"; // LOCAL
+  axios.defaults.baseURL = "https://technook-server.up.railway.app/"; // DEPLOY
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
