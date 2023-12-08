869f32eecffcbadbde8504906804554f8709d65b
barufe <belkisarufe@gmail.com>
2023-12-08 18:14:40 -0500

    create reviews

--- src/components/moduleHistoryOrderUser/ModuleHistoryOrderUser.jsx
+++ src/components/moduleHistoryOrderUser/ModuleHistoryOrderUser.jsx
@@ -10,7 +10,7 @@ const ModuleHistoryOrderUser =({idProp}) =>{
     const dispatch = useDispatch()
     const ordersById = useSelector((state) => state.ordersForUserId) || [];
     const [selectedOrder, setSelectedOrder] = useState(null);
-    
+    const isUser = useSelector((state) => state.isUser)
     
     useEffect(() => {
       if (id != undefined){
@@ -30,9 +30,12 @@ const ModuleHistoryOrderUser =({idProp}) =>{
     };
 
     return(
+      <div>
+      {isUser === "Admin" ? (
         <div>
         <h5>Orders list for user {ordersById[0]?.userName}:</h5>
         <table className="table table-hover">
+         
             <thead>
                 <tr>
                 <th className={styles.th} scope="col">ID Order</th>
@@ -67,6 +70,44 @@ const ModuleHistoryOrderUser =({idProp}) =>{
         />
       )}
     </div>
+      ) : (
+        <div>
+        <h5>Your orders {ordersById[0]?.userName}:</h5>
+        <table className="table table-hover">
+         
+            <thead>
+                <tr>
+                <th className={styles.th} scope="col">Number order</th>
+                <th className={styles.th} scope="col">Order Date</th>
+                <th className={styles.th} scope="col">Status</th>
+                <th className={styles.th} scope="col">Total Amount</th>
+                <th className={styles.th} scope="col">Order</th>
+                </tr>
+            </thead>
+                <tbody>
+                    {ordersById?.map((order) =>(<tr key={order.id}>
+                    <td className={styles.td}>{order.id}</td>
+                    <td className={styles.td}>{order.orderDate}</td>
+                    <td className={styles.td}>{order.deliveryStatus}</td>
+                    <td className={styles.td}>${parseFloat(order.totalPrice).toFixed(2)}</td>
+                    <td className={styles.td}><Link onClick={() => handleSeeDetail(JSON.parse(order.itemsCart))}>See detail</Link></td>
+                </tr>))}
+                {ordersById.length == 0 && 
+                <tr key="na">
+                <td>To date there are no purchase orders</td>
+                </tr>}
+              </tbody>
+        </table>
+        {selectedOrder && (
+        <OrderDetailUserByIdPopup
+          orderDetails={selectedOrder}
+          onClose={handleClosePopup}
+          idUser={idProp || id}
+        />
+      )}
+    </div>
+      )}
+        </div>
     )
 }
 
--- src/components/orderDetailUserByIdPopup/OrderDetailUserByIdPopup.jsx
+++ src/components/orderDetailUserByIdPopup/OrderDetailUserByIdPopup.jsx
@@ -1,39 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./orderDetailUserByIdPopup.module.css";
+import { useSelector } from "react-redux";
 
-const OrderDetailUserByIdPopup = ({ orderDetails, onClose }) => {
+const StarRating = ({ selectedStars, onSelectStar }) => {
+  const totalStars = 5;
+
   return (
     <div>
-      <div className={styles.overlay}></div>
-      <div className={styles.popup}>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th className={styles.th} scope="col">Id product</th>
-              <th className={styles.th} scope="col">Name product</th>
-              <th className={styles.th} scope="col">Price product</th>
-              <th className={styles.th} scope="col">Price on sale</th>
-              <th className={styles.th} scope="col">Stock</th>
-              <th className={styles.th} scope="col">Quantity</th>
-              <th className={styles.th} scope="col">Category</th>
-            </tr>
-          </thead>
-          <tbody>
-                {Array.isArray(orderDetails) && orderDetails.map((product) => (
+      {Array.from({ length: totalStars }).map((_, index) => (
+        <span
+          key={index}
+          className={index < selectedStars ? styles.selectedStar : styles.unselectedStar}
+          onClick={() => onSelectStar(index + 1)}
+        >
+          &#9733;
+        </span>
+      ))}
+    </div>
+  );
+};
+
+const OrderDetailUserByIdPopup = ({ orderDetails, onClose, idUser }) => {
+  const isUser = useSelector((state) => state.isUser);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [reviews, setReviews] = useState({});
+  
+  console.log(reviews);
+
+  const sendReview = () => {
+    console.log("Sending review for product:", reviews);
+
+    
+  };
+
+  return (
+    <div>
+      {isUser === "Admin" ? (
+        <div>
+          <div className={styles.overlay}></div>
+          <div className={styles.popup}>
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th className={styles.th} scope="col">Id product</th>
+                  <th className={styles.th} scope="col">Name product</th>
+                  <th className={styles.th} scope="col">Price product</th>
+                  <th className={styles.th} scope="col">Price on sale</th>
+                  <th className={styles.th} scope="col">Stock</th>
+                  <th className={styles.th} scope="col">Quantity</th>
+                  <th className={styles.th} scope="col">Category</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Array.isArray(orderDetails) &&
+                  orderDetails.map((product) => (
                     <tr key={product.id}>
-                    <td className={styles.td}>{product?.id}</td>
-                    <td className={styles.td}>{product?.nameProd}</td>
-                    <td className={styles.td}>${parseFloat(product?.price).toFixed(2)}</td>
-                    <td className={styles.td}>${parseFloat(product?.priceOnSale).toFixed(2)}</td>
-                    <td className={styles.td}>{product?.stock}</td>
-                    <td className={styles.td}>{product?.quantityProd}</td>
-                    <td className={styles.td}>{product?.category}</td>
+                      <td className={styles.td}>{product?.id}</td>
+                      <td className={styles.td}>{product?.nameProd}</td>
+                      <td className={styles.td}>${parseFloat(product?.price).toFixed(2)}</td>
+                      <td className={styles.td}>${parseFloat(product?.priceOnSale).toFixed(2)}</td>
+                      <td className={styles.td}>{product?.stock}</td>
+                      <td className={styles.td}>{product?.quantityProd}</td>
+                      <td className={styles.td}>{product?.category}</td>
                     </tr>
-                ))}
-         </tbody>
-        </table>
-        <button onClick={onClose}>Close</button>
-      </div>
+                  ))}
+              </tbody>
+            </table>
+            <button onClick={onClose}>Close</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className={styles.overlay}></div>
+          <div className={styles.popup}>
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th className={styles.th} scope="col">Product code</th>
+                  <th className={styles.th} scope="col">Quantity</th>
+                  <th className={styles.th} scope="col">Name product</th>
+                  <th className={styles.th} scope="col">Price product</th>
+                  <th className={styles.th} scope="col">Price on sale</th>
+                  <th className={styles.th} scope="col">Category</th>
+                  <th className={styles.th} scope="col">Review</th>
+                  <th className={styles.th} scope="col">Comment</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Array.isArray(orderDetails) &&
+                  orderDetails.map((product) => (
+                    <React.Fragment key={product.id}>
+                      <tr>
+                        <td className={styles.td}>{product?.id}</td>
+                        <td className={styles.td}>{product?.quantityProd}</td>
+                        <td className={styles.td}>{product?.nameProd}</td>
+                        <td className={styles.td}>${parseFloat(product?.price).toFixed(2)}</td>
+                        <td className={styles.td}>${parseFloat(product?.priceOnSale).toFixed(2)}</td>
+                        <td className={styles.td}>{product?.category}</td>
+                        <td className={styles.td}>
+                          <StarRating
+                            selectedStars={reviews[product.id]?.stars || 0}
+                            onSelectStar={(stars) =>
+                              setReviews({
+                                ...reviews,
+                                [product.id]: {
+                                  ...reviews[product.id],
+                                  stars: stars,
+                                },
+                              })
+                            }
+                          />
+                        </td>
+                        <td className={styles.td}>{product?.nameProd}</td>
+                      </tr>
+                    </React.Fragment>
+                  ))}
+              </tbody>
+            </table>
+            <button onClick={onClose}>Close</button>
+            <button onClick={sendReview}>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
