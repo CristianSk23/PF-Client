61702450d63c11737dac7f41c2847aa30d7a0c3c
BArufePerez <58121760+barufe@users.noreply.github.com>
2023-12-13 12:58:30 -0500

    Revert "Feature/routeprotection"

--- src/components/adminModuleUpdateUser/AdminModuleUpdateUser.jsx
+++ src/components/adminModuleUpdateUser/AdminModuleUpdateUser.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getUserById, setPageAdmin } from "../../redux/action/actions";
+import { updateUser, getUserById, setPageAdmin } from "../../redux/action/actions";
 import PopupGeneral from "../popupGeneral/PopupGeneral";
 import styles from "./adminModuleUpdateUser.module.css"
 import FloatingLabel from "react-bootstrap/FloatingLabel";
@@ -101,7 +101,7 @@ const AdminModuleUpdateUser = () => {
   })
 
 
-    dispatch(getUserById(user))
+    dispatch(updateUser(user))
     setShowConfirmation(true);
   }
   }; 
