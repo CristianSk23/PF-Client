1bff8602b6b5d2517f4df6a2bd532a0b82f8079e
barufe <belkisarufe@gmail.com>
2023-12-13 12:04:20 -0500

    add number and validate in payment

--- src/components/orderList/UserPurchaseHistory.jsx
+++ src/components/orderList/UserPurchaseHistory.jsx
@@ -33,8 +33,8 @@ export default function UserPurchaseHistory({ closeModal, data }) {
                   <td className={styles.td}>{i.id}</td>
                   <td className={styles.td}>{i.nameProd}</td>
                   <td className={styles.td}>{i.category}</td>
-                  <td className={styles.td}>${i.price?.toFixed(2)}</td>
-                  <td className={styles.td}>{i.priceOnSale?.toFixed(2) ?? "Not Available"}</td>
+                  <td className={styles.td}>${Number(i.price)?.toFixed(2)}</td>
+                  <td className={styles.td}>{Number(i.priceOnSale) > 0 ? Number(i.priceOnSale).toFixed(2) : Number(i.price).toFixed(2)}</td>
                   <td className={styles.td}>{i.stock}</td>
                   <td className={styles.td}>{i.quantityProd}</td>
                   </tr>
